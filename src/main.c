d6684a0c0a7bafe8c546313e5e139d573c805b5f
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-04-29 22:27:15 +0900

    Add simple echo reader

--- src/main.c
+++ src/main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
+#define LINE_MAX_LENGTH 256
+
 int main() {
-  puts("hello");
+  char line[LINE_MAX_LENGTH];
+  char last_char;
+  int char_index;
+
+  while (1) {
+    printf("> ");
+    char_index = 0;
+    while ((last_char = getchar()) != '\n') {
+      if (last_char == EOF) goto eof;
+      if (char_index == LINE_MAX_LENGTH) goto overflow;
+      line[char_index++] = last_char;
+    }
+    line[char_index] = '\0';
+
+    printf("%s\n", line);
+  }
+
+overflow:
+  puts("Line has oveflowed.");
+  goto exit;
+
+eof:
+  puts("");
+  goto exit;
+
+exit:
   return 0;
 }
